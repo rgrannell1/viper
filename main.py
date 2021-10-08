3e7d4ae6d731ba4e6321505a6d228373ad4f5e46
Róisín Grannell <r.grannell2@gmail.com>
2021-10-08 20:03:36 +0100

    include more information in frame

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 
 from abc import abstractmethod
-import json
+from pdb import set_trace
 import sys
+import json
+import inspect
+import datetime
 from types import FrameType
 from typing import Any, Callable, Optional, Union
+from cheap_repr import cheap_repr
+
+# -- -- -- Stack Frame Information -- -- -- #
 
 class ViperFrame:
+  """A stack-frame. Retrieve requested information.
+  """
   fn_name: Optional[str]
   fn_line: Optional[int]
   fn_file: Optional[str]
+  fn_filename: Optional[str]
 
-  parents: list[Any]
+  frame: FrameType
 
-  def __init__(self, event: str, frame: FrameType):
-    self.parents = []
+  def __init__(self, frame: FrameType):
+    self.frame = frame
+    self.fn_name = frame.f_code.co_name
+    self.fn_line = frame.f_lineno
+    self.fn_filename = frame.f_code.co_filename
 
     if frame is None:
       return
 
+  def arguments(self) -> dict[str, Any]:
+    argInfo = inspect.getargvalues(self.frame)
+
+    pairs = { }
+    for arg in argInfo.args:
+      pairs[arg] = cheap_repr(argInfo.locals[arg])
+
+    return pairs
+
+  def parents(self, transformer) -> list[Any]:
+    """List parent frames"""
+    parents = []
+
     # build up a list of parent stacks
-    tgt = frame.f_back
+    tgt = self.frame.f_back
     while True:
       if tgt is None:
         break
@@ -28,13 +53,17 @@ class ViperFrame:
       if tgt is None:
         break
 
-      self.parents.append(ViperFrame(event, tgt))
+      parents.append(ViperFrame(tgt).transform(transformer))
 
+    return parents
 
+  def transform(self, transformer) -> dict[str, Any]:
+    return transformer().transform(self)
 
-
+# -- -- -- Event Parent Class -- -- -- #
 
 class ViperEvent:
+  """Includes information about a stack frame and event captured by settrace."""
   frame: ViperFrame
   event: str
   arg: Any
@@ -65,20 +94,16 @@ class ViperEvent:
     """An abstract method; each subclass should be representable as a dictionary"""
     raise NotImplementedError('dict() not implemented')
 
-  def __repr__(self) -> str:
-    """Print a JSON representation of an event"""
-    return 'no' #json.dumps(self.__dict__())
 
 
-
-
+# -- -- -- Event Classes -- -- -- #
 
 # TODO improve interface; frame as a class, event as an enum, arg as a safe json repr
 class ViperEventCall(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = ViperFrame(event, frame)
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -89,9 +114,9 @@ class ViperEventCall(ViperEvent):
 
 class ViperEventLine(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = frame
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -102,9 +127,9 @@ class ViperEventLine(ViperEvent):
 
 class ViperEventReturn(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = frame
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -116,9 +141,9 @@ class ViperEventReturn(ViperEvent):
 
 class ViperEventException(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = frame
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -130,9 +155,9 @@ class ViperEventException(ViperEvent):
 
 class ViperEventCCall(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = frame
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -144,9 +169,9 @@ class ViperEventCCall(ViperEvent):
 
 class ViperEventCReturn(ViperEvent):
   def __init__(self, frame, event, arg):
-    self.frame = frame
+    self.frame = ViperFrame(frame)
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -160,7 +185,7 @@ class ViperEventCException(ViperEvent):
   def __init__(self, frame, event, arg):
     self.frame = frame
     self.event = event
-    self.arg = arg
+    self.arg = cheap_repr(arg)
 
   def __dict__(self) -> dict[str, Any]:
     return {
@@ -169,14 +194,15 @@ class ViperEventCException(ViperEvent):
       'arg': self.arg
     }
 
+# -- -- -- Function Types -- -- -- #
 
-# function parameter types
 ViperEvents = Union[ViperEventCall, ViperEventLine, ViperEventReturn,
                     ViperEventException, ViperEventCCall, ViperEventCReturn, ViperEventCException]
 
 ViperFilterFunc = Callable[[ViperEvents], bool]
 ViperWriterFunc = Callable[[ViperEvents], Any]
 
+# -- -- -- Core Viper Class -- -- -- #
 
 class Viper:
   """Trace application calls, return values, and exceptions, and write the results to a callback function."""
@@ -185,9 +211,13 @@ class Viper:
   def filter(self, event: ViperEvents) -> bool:
     raise NotImplementedError("you need to implement filter")
 
-  def writer(self, event) -> None:
+  @abstractmethod
+  def transform(self, event: ViperEvents) -> dict[str, Any]:
+    raise NotImplementedError("you need to implement transform")
+
+  def writer(self, event: dict[str, Any]) -> None:
     """By default, Viper will print a JSON-representation of events to stderr"""
-    print(event, file=sys.stderr)
+    print(json.dumps(event), file=sys.stderr)
 
   def start_trace(self) -> None:
     """Start tracing application"""
@@ -196,8 +226,11 @@ class Viper:
 
       # pass to user trace function
       viper_event = ViperEvent.new(frame, event, arg)
+
+      # filter an event
       if self.filter(viper_event):
-        self.writer(viper_event)
+        # write an event
+        self.writer(self.transform(viper_event))
 
       self.start_trace()
 
@@ -209,9 +242,46 @@ class Viper:
 
 
 
+# -- -- --  Frame Transformers -- -- -- #
+
+class ViperFrameTransformer:
+  @abstractmethod
+  def transform(self, frame: ViperFrame) -> dict[str, Any]:
+    raise NotImplementedError("transform must be implemented")
+
+
+class ViperParentFrame(ViperFrameTransformer):
+  def transform(self, frame: ViperFrame) -> dict[str, Any]:
+    return {
+      'type': 'parent-frame',
+      'fn_name': frame.fn_name,
+      'fn_line': frame.fn_line,
+      'fn_filename': frame.fn_filename,
+      'time': datetime.datetime.now().isoformat(),
+      'epoch': datetime.datetime.now().timestamp()
+    }
+
+class ViperChildFrame(ViperFrameTransformer):
+  def transform(self, frame: ViperFrame) -> dict[str, Any]:
+
+    return {
+      'type': 'child-frame',
+      'parents': frame.parents(ViperParentFrame),
+      'fn_name': frame.fn_name,
+      'fn_line': frame.fn_line,
+      'args': [
+        frame.arguments()
+      ],
+      'fn_filename': frame.fn_filename,
+      'time': datetime.datetime.now().isoformat(),
+      'epoch': datetime.datetime.now().timestamp()
+    }
+
+# -- -- -- Test Code -- -- -- #
+
+
 def a(num):
   def c(x):
-    print(x)
     return x
 
   for idx in range(10):
@@ -219,15 +289,24 @@ def a(num):
 
   return 2
 
-def filter(event: ViperEvent):
-  return isinstance(event, ViperEventCall) and event.frame
 
-class PitViper(Viper):
+class SimpleSummary(Viper):
   def filter(self, event):
-    return True
+    return isinstance(event, ViperEventCall) and event.frame
+
+  def transform(self, event):
+    frame = event.frame.transform(ViperChildFrame)
+    arg = event.arg
+    event = event.event
+
+    return {
+      'event': event,
+      'frame': frame,
+      'arg': arg
+    }
 
 def main():
-  viper = PitViper()
+  viper = SimpleSummary()
 
   viper.start_trace()
   a(1)
